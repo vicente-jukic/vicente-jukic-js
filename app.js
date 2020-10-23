58651007e3f80351753d0d2f5b424a28f820a481
vicente-jukic <vnjukic@gmail.com>
2020-10-23 20:40:58 -0300

    Strings

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 JS Excercises Week 4 
 **********************/
 
-// Variables & Operators //
+//****** Variables & Operators ******//
 
 // a)
 
@@ -27,3 +27,50 @@ var surname = 'Jukic';
 var nameLength = firstName.length + surname.length;
 
 console.log(nameLength);
+
+
+//****** Strings ******//
+
+// a)
+
+var upperCaseName = 'jennifer aninston'.toUpperCase();
+
+console.log(upperCaseName);
+
+// b)
+
+var firstWholeWord = 'Adaptation'
+var firstCharacters = firstWholeWord.substring(0, 5);
+
+console.log(firstCharacters);
+
+// c)
+
+var secondWholeWord = 'Skateboard';
+var lastCharacters = secondWholeWord.substring(7, 10);
+
+console.log(lastCharacters);
+
+// d)
+
+var uglyWord = 'aPOcaLyPsE';
+var prettyWord = uglyWord.substring(0,1).toUpperCase() + uglyWord.substring(1).toLowerCase();
+
+console.log(prettyWord);
+
+// e)
+
+var spaceddWord = 'Light board';
+var spaceIndex = spaceddWord.indexOf(' ');
+
+console.log(spaceIndex);
+
+// f)
+
+var tenLettersWords = 'waterMElon techNOLogy';
+var spaceIndex2 = tenLettersWords.indexOf(' ');
+var tenLettersWordsUC = tenLettersWords.substring(0,1).toUpperCase() + tenLettersWords.substring(1,spaceIndex2).toLowerCase() + tenLettersWords.substring(spaceIndex2,spaceIndex2+1) + tenLettersWords.substring(spaceIndex2+1, spaceIndex2+2).toUpperCase() + tenLettersWords.substring(spaceIndex2+2).toLowerCase();
+
+console.log(tenLettersWordsUC);
+
+
