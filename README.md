efe67b5eb24388bf0ff4cc2375fd99b856e82f44
vicente-jukic <vnjukic@gmail.com>
2020-10-23 19:54:39 -0300

    Variables & Operatorss

---
+++ README.md
@@ -0,0 +1,15 @@
+# Become a Software Developer - Week 4 - JavaScript
+
+** Version 1.0.0 **
+
+List of excercises:
+- Variables & Operators
+- Strings
+- Arrays
+- If Else
+- For
+- Functions
+
+## Contributors
+
+Vicente N. Jukic
---
+++ app.js
@@ -0,0 +1,29 @@
+/*********************
+JS Excercises Week 4 
+**********************/
+
+// Variables & Operators //
+
+// a)
+
+var firstNumber = 5;
+var secondNumber = 9;
+var thirdNumber = firstNumber + secondNumber;
+
+console.log(thirdNumber);
+
+// b)
+
+var firstString = 'Hello';
+var secondString = 'World';
+var thirdString = firstString + ' ' + secondString;
+
+console.log(thirdString);
+
+// c)
+
+var firstName = 'Vicente';
+var surname = 'Jukic';
+var nameLength = firstName.length + surname.length;
+
+console.log(nameLength);
